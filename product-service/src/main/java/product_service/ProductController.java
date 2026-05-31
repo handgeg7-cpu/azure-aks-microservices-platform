4914f2a3ed106fc13c86e857e3564527427890da
grhandge <handgeg7@example.com>
2026-05-31 16:54:37 +0530

    Added congratulations landing page

--- product-service/src/main/java/product_service/ProductController.java
+++ product-service/src/main/java/product_service/ProductController.java
@@ -1,14 +1,14 @@
 package product_service;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class ProductController {
 
     @GetMapping("/products")
     public String products() {
-        return "Product Service Running on AKS Congrate Ganesh You did it congrates! YOU Got (Package 28 LPA)";
+        return "forward:/index.html";
     }
 
     @GetMapping("/health")
